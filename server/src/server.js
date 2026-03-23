43632bafb6359b5b6ab8ab8bf2dcf13da2884fe9
codecamp-th <codecamp@relearnsolution.co.th>
2026-03-23 16:51:18 +0700

    initial socket/io

--- server/src/server.js
+++ server/src/server.js
@@ -4,17 +4,30 @@ import cors from "cors";
 import authRoute from "./routes/auth.route.js";
 import errHandle from "./middlewares/errHandle.js";
 import notFound from "./middlewares/notFound.js";
+import http from "http";
+import { Server } from "socket.io";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-
 app.use(express.json());
 app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "http://localhost:5173" },
+});
 
 app.use("/auth", authRoute);
 
 app.use(notFound);
 app.use(errHandle);
-app.listen(PORT, () => {
+
+io.on("connection", (socket) => {
+  socket.on("hello", (arg) => {
+    console.log(arg); // world
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
